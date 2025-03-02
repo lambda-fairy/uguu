ac7ae6bdae1bf57e10849781dd7ab658ad79954a
G2 <ke0bhogsg@gmail.com>
2025-03-02 01:40:11 -0600

    Updated to edition 2024, updated Rodio (#2)
    * Updated to edition 2024, updated Rodio
    
    * Ran `cargo update`

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use std::{io, process};
+#![windows_subsystem = "windows"]
+
+use std::{io::Cursor, process};
+
+use rodio::{Decoder, OutputStream, Sink};
 
 static UGUU: &[u8] = include_bytes!("uguu_1.mp3");
 
 fn main() {
-    let device = if let Some(device) = rodio::default_output_device() {
-        device
+    let (_stream, stream_handle) = if let Ok((_stream, stream_handle)) = OutputStream::try_default() {
+        (_stream, stream_handle)
     } else {
         eprintln!("No audio output device available");
         process::exit(1);
     };
 
-    let sink = rodio::play_once(&device, io::Cursor::new(UGUU)).unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    sink.set_volume(0.2);
+
+    sink.append(Decoder::new(Cursor::new(UGUU)).unwrap());
+
     sink.sleep_until_end();
 }
