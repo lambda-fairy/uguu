386da7d8a1d5f1b25bda9d87fdf0eb1a98c769ae
Chris Wong <lambda.fairy@gmail.com>
2025-03-02 18:45:59 +1100

    Use `Box<dyn Error>` instead of unwrapping

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 #![windows_subsystem = "windows"]
 
-use std::{io::Cursor, process};
+use std::{error::Error, io::Cursor};
 
 use rodio::{Decoder, OutputStream, Sink};
 
 static UGUU: &[u8] = include_bytes!("uguu_1.mp3");
 
-fn main() {
-    let (_stream, stream_handle) = if let Ok((_stream, stream_handle)) = OutputStream::try_default() {
-        (_stream, stream_handle)
-    } else {
-        eprintln!("No audio output device available");
-        process::exit(1);
-    };
-
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
+fn main() -> Result<(), Box<dyn Error>> {
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
     sink.set_volume(0.2);
-
-    sink.append(Decoder::new(Cursor::new(UGUU)).unwrap());
-
+    sink.append(Decoder::new(Cursor::new(UGUU))?);
     sink.sleep_until_end();
+    Ok(())
 }
